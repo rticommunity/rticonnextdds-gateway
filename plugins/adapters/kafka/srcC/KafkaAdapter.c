0ede373dbf3dd0111cf6425c1f5d5d6d92af29ae
Kyoungho An <kyoungho@rti.com>
2021-10-07 10:44:18 -0500

    refactored kafka plugin

--- plugins/adapters/kafka/srcC/KafkaAdapter.c
+++ plugins/adapters/kafka/srcC/KafkaAdapter.c
@@ -24,22 +24,24 @@
  * Entry point to the Kafka adapter plugin
  */
 RTI_USER_DLL_EXPORT
-struct RTI_RoutingServiceAdapterPlugin *
-RTI_RS_Kafka_AdapterPlugin_create(
-    const struct RTI_RoutingServiceProperties *properties,
-    RTI_RoutingServiceEnvironment *env)
+struct RTI_RoutingServiceAdapterPlugin *RTI_RS_Kafka_AdapterPlugin_create(
+        const struct RTI_RoutingServiceProperties *properties,
+        RTI_RoutingServiceEnvironment *env)
 {
-
     struct RTI_RS_KafkaAdapterPlugin *adapter = NULL;
-    struct RTI_RoutingServiceVersion version = {1, 0, 0, 0};
+    struct RTI_RoutingServiceVersion version = { 0, 0, 0, 1 };
 
-    RTI_RoutingServiceLogger_log(RTI_ROUTING_SERVICE_VERBOSITY_DEBUG, "%s", __func__);
+    RTI_RoutingServiceLogger_log(
+            RTI_ROUTING_SERVICE_VERBOSITY_DEBUG,
+            "%s",
+            __func__);
 
     adapter = calloc(1, sizeof(struct RTI_RS_KafkaAdapterPlugin));
 
-    if (adapter == NULL)
-    {
-        //RTI_RoutingServiceEnvironment_set_error(env, "RTI_RS_KafkaAdpterPlugin memory allocation error");
+    if (adapter == NULL) {
+        RTI_RoutingServiceEnvironment_set_error(
+                env,
+                "RTI_RS_KafkaAdpterPlugin memory allocation error");
         return NULL;
     }
 
@@ -49,83 +51,88 @@ RTI_RS_Kafka_AdapterPlugin_create(
 
     /* Assign the function pointers */
 
-    adapter->base.adapter_plugin_delete =
-        RTI_RS_KafkaAdapterPlugin_delete;
+    adapter->base.adapter_plugin_delete = RTI_RS_KafkaAdapterPlugin_delete;
 
     adapter->base.adapter_plugin_create_connection =
-        RTI_RS_KafkaAdapterPlugin_create_connection;
+            RTI_RS_KafkaAdapterPlugin_create_connection;
     adapter->base.adapter_plugin_delete_connection =
-        RTI_RS_KafkaAdapterPlugin_delete_connection;
+            RTI_RS_KafkaAdapterPlugin_delete_connection;
 
-    adapter->base.connection_create_session =
-        NULL;
-    adapter->base.connection_delete_session =
-        NULL;
+    adapter->base.connection_create_session = NULL;
+    adapter->base.connection_delete_session = NULL;
 
     adapter->base.connection_create_stream_writer =
-        RTI_RS_KafkaConnection_create_stream_writer;
+            RTI_RS_KafkaConnection_create_stream_writer;
     adapter->base.connection_delete_stream_writer =
-        RTI_RS_KafkaConnection_delete_stream_writer;
+            RTI_RS_KafkaConnection_delete_stream_writer;
 
     adapter->base.connection_create_stream_reader =
-        RTI_RS_KafkaConnection_create_stream_reader;
+            RTI_RS_KafkaConnection_create_stream_reader;
     adapter->base.connection_delete_stream_reader =
-        RTI_RS_KafkaConnection_delete_stream_reader;
+            RTI_RS_KafkaConnection_delete_stream_reader;
 
-    adapter->base.stream_writer_write =
-        RTI_RS_KafkaStreamWriter_write;
+    adapter->base.stream_writer_write = RTI_RS_KafkaStreamWriter_write;
 
-    adapter->base.stream_reader_read =
-        RTI_RS_KafkaStreamReader_read;
+    adapter->base.stream_reader_read = RTI_RS_KafkaStreamReader_read;
     adapter->base.stream_reader_return_loan =
-        RTI_RS_KafkaStreamReader_return_loan;
-    adapter->base.stream_reader_update =
-        NULL;
+            RTI_RS_KafkaStreamReader_return_loan;
+    adapter->base.stream_reader_update = NULL;
 
-    return (struct RTI_RoutingServiceAdapterPlugin *)adapter;
+    return (struct RTI_RoutingServiceAdapterPlugin *) adapter;
 }
 
 void RTI_RS_KafkaAdapterPlugin_delete(
-    struct RTI_RoutingServiceAdapterPlugin *adapter,
-    RTI_RoutingServiceEnvironment *env)
+        struct RTI_RoutingServiceAdapterPlugin *adapter,
+        RTI_RoutingServiceEnvironment *env)
 {
-    RTI_RoutingServiceLogger_log(RTI_ROUTING_SERVICE_VERBOSITY_DEBUG, "%s", __func__);
+    RTI_RoutingServiceLogger_log(
+            RTI_ROUTING_SERVICE_VERBOSITY_DEBUG,
+            "%s",
+            __func__);
 
     free(adapter);
 }
 
 /* Create a Kafka adapter connection with configurations */
-RTI_RoutingServiceConnection
-RTI_RS_KafkaAdapterPlugin_create_connection(
-    struct RTI_RoutingServiceAdapterPlugin *adapter,
-    const char *routing_service_name,
-    const char *routing_service_group_name,
-    const struct RTI_RoutingServiceStreamReaderListener *input_disc_listener,
-    const struct RTI_RoutingServiceStreamReaderListener *output_disc_listener,
-    const struct RTI_RoutingServiceTypeInfo **registeredTypes,
-    int registeredTypeCount,
-    const struct RTI_RoutingServiceProperties *properties,
-    RTI_RoutingServiceEnvironment *env)
+RTI_RoutingServiceConnection RTI_RS_KafkaAdapterPlugin_create_connection(
+        struct RTI_RoutingServiceAdapterPlugin *adapter,
+        const char *routing_service_name,
+        const char *routing_service_group_name,
+        const struct RTI_RoutingServiceStreamReaderListener
+                *input_disc_listener,
+        const struct RTI_RoutingServiceStreamReaderListener
+                *output_disc_listener,
+        const struct RTI_RoutingServiceTypeInfo **registeredTypes,
+        int registeredTypeCount,
+        const struct RTI_RoutingServiceProperties *properties,
+        RTI_RoutingServiceEnvironment *env)
 {
     struct RTI_RS_KafkaConnection *connection;
 
-    RTI_RoutingServiceLogger_log(RTI_ROUTING_SERVICE_VERBOSITY_DEBUG, "%s", __func__);
+    RTI_RoutingServiceLogger_log(
+            RTI_ROUTING_SERVICE_VERBOSITY_DEBUG,
+            "%s",
+            __func__);
 
     connection = calloc(1, sizeof(struct RTI_RS_KafkaConnection));
 
-    if (connection == NULL)
-    {
-        //RTI_RoutingServiceEnvironment_set_error(env, "RTI_RS_KafkaConnection memory allocation error");
+    if (connection == NULL) {
+        RTI_RoutingServiceEnvironment_set_error(
+                env,
+                "RTI_RS_KafkaConnection memory allocation error");
         return NULL;
     }
 
-    connection->adapter = (struct RTI_RS_KafkaAdapterPlugin *)adapter;
+    connection->adapter = (struct RTI_RS_KafkaAdapterPlugin *) adapter;
 
-    connection->bootstrap_servers = RTI_RoutingServiceProperties_lookup_property(
-        properties, "bootstrap.servers");
-    if (connection->bootstrap_servers == NULL)
-    {
-        //RTI_RoutingServiceEnvironment_set_error(env, "bootstrap.servers missing property brokers");
+    connection->bootstrap_servers =
+            RTI_RoutingServiceProperties_lookup_property(
+                    properties,
+                    "bootstrap.servers");
+    if (connection->bootstrap_servers == NULL) {
+        RTI_RoutingServiceEnvironment_set_error(
+                env,
+                "bootstrap.servers missing property brokers");
         return NULL;
     }
 
@@ -133,14 +140,16 @@ RTI_RS_KafkaAdapterPlugin_create_connection(
 }
 
 void RTI_RS_KafkaAdapterPlugin_delete_connection(
-    struct RTI_RoutingServiceAdapterPlugin *adapter,
-    RTI_RoutingServiceConnection connection,
-    RTI_RoutingServiceEnvironment *env)
+        struct RTI_RoutingServiceAdapterPlugin *adapter,
+        RTI_RoutingServiceConnection connection,
+        RTI_RoutingServiceEnvironment *env)
 {
-    RTI_RoutingServiceLogger_log(RTI_ROUTING_SERVICE_VERBOSITY_DEBUG, "%s", __func__);
+    RTI_RoutingServiceLogger_log(
+            RTI_ROUTING_SERVICE_VERBOSITY_DEBUG,
+            "%s",
+            __func__);
 
     free(connection);
 }
-
 
 #undef ROUTER_CURRENT_SUBMODULE
--- plugins/adapters/kafka/srcC/KafkaAdapter.h
+++ plugins/adapters/kafka/srcC/KafkaAdapter.h
@@ -22,38 +22,38 @@
 #include "routingservice/routingservice_service.h"
 
 #include "KafkaConnection.h"
-#include "KafkaStreamWriter.h"
 #include "KafkaStreamReader.h"
+#include "KafkaStreamWriter.h"
 
 struct RTI_RS_KafkaAdapterPlugin {
     struct RTI_RoutingServiceAdapterPlugin base;
 };
 
 RTI_USER_DLL_EXPORT
-struct RTI_RoutingServiceAdapterPlugin *
-RTI_RS_Kafka_AdapterPlugin_create(
-    const struct RTI_RoutingServiceProperties *properties,
-    RTI_RoutingServiceEnvironment *env);
+struct RTI_RoutingServiceAdapterPlugin *RTI_RS_Kafka_AdapterPlugin_create(
+        const struct RTI_RoutingServiceProperties *properties,
+        RTI_RoutingServiceEnvironment *env);
 
 void RTI_RS_KafkaAdapterPlugin_delete(
-    struct RTI_RoutingServiceAdapterPlugin *adapter,
-    RTI_RoutingServiceEnvironment *env);
+        struct RTI_RoutingServiceAdapterPlugin *adapter,
+        RTI_RoutingServiceEnvironment *env);
 
-RTI_RoutingServiceConnection
-RTI_RS_KafkaAdapterPlugin_create_connection(
-    struct RTI_RoutingServiceAdapterPlugin *adapter,
-    const char *routing_service_name,
-    const char *routing_service_group_name,
-    const struct RTI_RoutingServiceStreamReaderListener *input_disc_listener,
-    const struct RTI_RoutingServiceStreamReaderListener *output_disc_listener,
-    const struct RTI_RoutingServiceTypeInfo **registeredTypes,
-    int registeredTypeCount,
-    const struct RTI_RoutingServiceProperties *properties,
-    RTI_RoutingServiceEnvironment *env);
+RTI_RoutingServiceConnection RTI_RS_KafkaAdapterPlugin_create_connection(
+        struct RTI_RoutingServiceAdapterPlugin *adapter,
+        const char *routing_service_name,
+        const char *routing_service_group_name,
+        const struct RTI_RoutingServiceStreamReaderListener
+                *input_disc_listener,
+        const struct RTI_RoutingServiceStreamReaderListener
+                *output_disc_listener,
+        const struct RTI_RoutingServiceTypeInfo **registeredTypes,
+        int registeredTypeCount,
+        const struct RTI_RoutingServiceProperties *properties,
+        RTI_RoutingServiceEnvironment *env);
 
 void RTI_RS_KafkaAdapterPlugin_delete_connection(
-    struct RTI_RoutingServiceAdapterPlugin *adapter,
-    RTI_RoutingServiceConnection connection,
-    RTI_RoutingServiceEnvironment *env);
+        struct RTI_RoutingServiceAdapterPlugin *adapter,
+        RTI_RoutingServiceConnection connection,
+        RTI_RoutingServiceEnvironment *env);
 
 #endif /* KafkaAdapter_h */
