6cefca2123c5d9c65bac81436e3574a790f267e2
Angel Martinez <angel@rti.com>
2022-07-13 22:48:53 +0200

    Update minimum version for building

--- resource/cmake/CommonFunctions.cmake
+++ resource/cmake/CommonFunctions.cmake
@@ -261,7 +261,7 @@ macro(rtigw_init_globals)
 
     list(APPEND CMAKE_MODULE_PATH "${RTICMAKE_UTILS_MODULES_DIR}")
 
-    rtigw_configure_connextdds(6.0.1)
+    rtigw_configure_connextdds(6.1.0)
 
     if (RTIGATEWAY_ENABLE_TESTS)
         enable_testing()
